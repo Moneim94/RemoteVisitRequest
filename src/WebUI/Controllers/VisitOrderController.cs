7571652d10e2a43c1f406d69858c6a3a24be40ed
Moneim <Moneim@DESKTOP-VARQG2R>
2023-01-23 11:42:24 +0200

    jkl

--- src/WebUI/Controllers/VisitOrderController.cs
+++ src/WebUI/Controllers/VisitOrderController.cs
@@ -1,14 +1,21 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Order.Application.RemoteOrder.Commands.CreateRemoteOrder;
 using Order.WebUI.Controllers;
 
 namespace WebUI.Controllers;
-[Route("api/[controller]")]
+[Route("/api/[controller]")]
 [ApiController]
-public class VisiteOrderController : ApiControllerBase
+ 
+public class VisitOrderController : ApiControllerBase
 {
-    [HttpGet]
+    [HttpGet("Test")]
+    public IActionResult Test()
+    {
+        return Content("API Runnning Successfuly..");
+    }
+    [HttpPost("CreateVisitOrder")]
     public async  Task<ActionResult<int>> CreateVisitOrder(CreateVisitOrderCommand createRemoteOrderCommand)
     {
        return await Mediator.Send(createRemoteOrderCommand);
